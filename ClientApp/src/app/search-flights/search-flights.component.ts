104caf874aa99966a94f73cdaee46092810d2a4e
Manali <manali.vashi@tech9.com>
2023-09-08 14:15:35 -0500

    display flights name

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SearchFlightsComponent {
 
+  searchResult: any = [
+    "AirFlightLine",
+    "EuphoniousFlightLine",
+    "GeekStarFlightLine"
+  ]
+
 }

8c21eb08c9d24550d5fd9fb1b4bed5c9214f07ea
Manali <manali.vashi@tech9.com>
2023-09-08 10:59:24 -0500

    add search flight component

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'app-search-flights', component: SearchFlightsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flights',
+  templateUrl: './search-flights.component.html',
+  styleUrls: ['./search-flights.component.scss']
+})
+export class SearchFlightsComponent {
+
+}

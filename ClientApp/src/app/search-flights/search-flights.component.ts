08959a2c0e88f27843f1d009b2e423567f5c2ef1
Manali <manali.vashi@tech9.com>
2023-10-09 12:10:01 -0500

    use flight service

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FlightService } from '../api/services';
+import { FlightRm } from '../api/models';
 
 @Component({
   selector: 'app-search-flights',
@@ -7,64 +9,17 @@ import { Component } from '@angular/core';
 })
 export class SearchFlightsComponent {
 
-  searchResult: FlightRM[] = [
-    {
-      id: 1,
-      airline: "AirLine1",
-      remainingNumberOfSeats: 500,
-      departure: {
-        time: Date.now().toString(),
-        place: "Surat"
-      },
-      price: "350",
-      arrival:{
-        time: Date.now().toString(),
-        place: "Mumbai"
-      }
-    },
-    {
-      id:2,
-      airline: "AirLine2",
-      remainingNumberOfSeats: 600,
-      departure: {
-        time: Date.now().toString(),
-        place: "Mumbai"
-      },
-      price: "450",
-      arrival:{
-        time: Date.now().toString(),
-        place: "Banglore"
-      }
-    },
-    {
-      id:3,
-      airline: "AirLine3",
-      remainingNumberOfSeats: 200,
-      departure: {
-        time: Date.now().toString(),
-        place: "Pune"
-      },
-      price: "220",
-      arrival:{
-        time: Date.now().toString(),
-        place: "Delhi"
-      }
-    },
-    {
-      id: 4,
-      airline: "AirLine4",
-      remainingNumberOfSeats: 500,
-      departure: {
-        time: Date.now().toString(),
-        place: "Ajmer"
-      },
-      price: "890",
-      arrival:{
-        time: Date.now().toString(),
-        place: "Surat"
-      }
-   }
-  ]
+  searchResult: FlightRm[] = []
+
+  constructor(private flightService: FlightService)
+  {
+    this.flightService.flightGet()
+      .subscribe(response => this.searchResult = response, this.HandleError);
+  }
+
+  private HandleError(err: any) {
+    console.log(err);
+  }
 
 }
 
@@ -72,11 +27,11 @@ export interface TimePlaceRM{
   place: string;
   time: string;
 }
-export interface FlightRM{
-  airline: string;
-  arrival: TimePlaceRM;
-  departure: TimePlaceRM;
-  price: string;
-  remainingNumberOfSeats: number;
-  id: number;
-}
+// export interface FlightRm{
+//   airline: string;
+//   arrival: TimePlaceRM;
+//   departure: TimePlaceRM;
+//   price: string;
+//   remainingNumberOfSeats: number;
+//   id: number;
+// }
